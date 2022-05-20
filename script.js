ad2273f3d9040be8ab52e3c71c1be60dad600d47
Lxgola <leonardoglacerda@gmail.com>
2022-05-19 21:57:58 -0300

    mudar cor de fundo

--- script.js
+++ script.js
@@ -1,15 +1,33 @@
-const na = Math.floor(Math.random() * 101);
 // Pegar numero aleatorio
+const na = Math.floor(Math.random() * 101);
+
+//errar e acertar
+function acertou(){
+    document.body.style.backgroundColor = "white";
+    setTimeout(() => { document.body.style.backgroundColor = "green"; }, 700);
+};
+function erroua(){
+    document.body.style.backgroundColor = "white";
+    setTimeout(() => { document.body.style.backgroundColor = "red"; }, 700);
+};
+function erroub(){
+    document.body.style.backgroundColor = "white";
+    setTimeout(() => { document.body.style.backgroundColor = "red"; }, 700);
+}
 function salvarn(){
     var n = document.querySelector('#numero').value;
-    // Apos 1 hora e 11 minutos, consegui sozinho resolver o problema que a pagina recarregava e perdia o "n", mas trocando o input type pra button e adicionando evento de onclick, resolveu
+    // Apos 1 hora e 11 minutos, consegui sozinho resolver o problema que a pagina recarregava e perdia o "n", mas trocando o input type pra button e adicionando evento de onclick resolveu
+
     if (n > na){
-        console.log("Seu número foi muito alto");
+        erroua();
+        console.log('muito alto')
     }
     else if (n < na){
-        console.log("Seu número foi muito baixo");
+        erroub();
+        console.log('muito baixo')
     }
     else{
-        console.log("Voce acertou");
+        acertou();
     }
+
 };
