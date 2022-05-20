5c16449827e72f46dba1b34bd4662e75adc23c62
Lxgola <leonardoglacerda@gmail.com>
2022-05-20 20:46:57 -0300

    adicionar texto ao errar

--- script.js
+++ script.js
@@ -1,18 +1,45 @@
 // Pegar numero aleatorio
 const na = Math.floor(Math.random() * 101);
 
+
 //errar e acertar
 function acertou(){
     document.body.style.backgroundColor = "white";
     setTimeout(() => { document.body.style.backgroundColor = "green"; }, 700);
-};
+
+
+    var h1Novo = document.createElement("h1");
+    var conteudoNovo = document.createTextNode("Acertou");
+    h1Novo.setAttribute("id", "h1novoa");
+    h1Novo.appendChild(conteudoNovo);
+
+    setTimeout(() => { document.body.appendChild(h1Novo); }, 700);
+    
+}
+;
 function erroua(){
     document.body.style.backgroundColor = "white";
-    setTimeout(() => { document.body.style.backgroundColor = "red"; }, 700);
+    setTimeout(() => { document.body.style.backgroundColor = "rgb(231, 47, 47)"; }, 700);
+
+    var h1Novo = document.createElement("h1");
+    var conteudoNovo = document.createTextNode("Tente um número mais baixo");
+    h1Novo.setAttribute("id", "h1novoea");
+    h1Novo.appendChild(conteudoNovo);
+
+    setTimeout(() => { document.body.appendChild(h1Novo); }, 700);
+
 };
 function erroub(){
     document.body.style.backgroundColor = "white";
-    setTimeout(() => { document.body.style.backgroundColor = "red"; }, 700);
+    setTimeout(() => { document.body.style.backgroundColor = "rgb(231, 47, 47)"; }, 700);
+
+    var h1Novo = document.createElement("h1");
+    var conteudoNovo = document.createTextNode("Tente um número mais alto");
+    h1Novo.setAttribute("id", "h1novoeb");
+    h1Novo.appendChild(conteudoNovo);
+
+    setTimeout(() => { document.body.appendChild(h1Novo); }, 700);
+
 }
 function salvarn(){
     var n = document.querySelector('#numero').value;
