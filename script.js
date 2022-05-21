798dbd044ec8d77b21260d95f0e0a11a3366d82b
Lxgola <leonardoglacerda@gmail.com>
2022-05-20 21:30:13 -0300

    nova pagina de acerto

--- script.js
+++ script.js
@@ -1,20 +1,11 @@
-// Pegar numero aleatorio
 const na = Math.floor(Math.random() * 101);
 
+var tentativas = 0
 
-//errar e acertar
 function acertou(){
-    document.body.style.backgroundColor = "white";
-    setTimeout(() => { document.body.style.backgroundColor = "green"; }, 700);
-
-
-    var h1Novo = document.createElement("h1");
-    var conteudoNovo = document.createTextNode("Acertou");
-    h1Novo.setAttribute("id", "h1novoa");
-    h1Novo.appendChild(conteudoNovo);
-
-    setTimeout(() => { document.body.appendChild(h1Novo); }, 700);
-    
+    document.body.style.backgroundColor = "white"; 
+    setTimeout(() => {
+        window.location.href = "acerto.html"; }, 700);
 }
 ;
 function erroua(){
@@ -41,17 +32,17 @@ function erroub(){
     setTimeout(() => { document.body.appendChild(h1Novo); }, 700);
 
 }
+
 function salvarn(){
     var n = document.querySelector('#numero').value;
-    // Apos 1 hora e 11 minutos, consegui sozinho resolver o problema que a pagina recarregava e perdia o "n", mas trocando o input type pra button e adicionando evento de onclick resolveu
 
     if (n > na){
         erroua();
-        console.log('muito alto')
+        tentativas += 1
     }
     else if (n < na){
         erroub();
-        console.log('muito baixo')
+        tentativas += 1
     }
     else{
         acertou();
